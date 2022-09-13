6f16d13ae3c0e1d2695361de3e005c6fd9075ef8
Flawid DSouza <me@flawiddsouza.com>
2022-09-13 14:10:45 +0530

    feat(ui): Response Panel > Preview > Add "Possible causes" for "Error: Couldn't resolve host name" + include browser extension links for cors bypass

--- ui/src/components/ResponsePanel.vue
+++ ui/src/components/ResponsePanel.vue
@@ -41,7 +41,16 @@
                 <template v-if="response.statusText !== 'Error'">
                     <CodeMirrorResponsePanelPreview :model-value="bufferToJSONString(response.buffer)" />
                 </template>
-                <div class="content-box" v-else>{{ response.error }}</div>
+                <div class="content-box" v-else>
+                    <div>{{ response.error }}</div>
+                    <div style="margin-top: 1.5rem; width: 30rem;">
+                        <div style="margin-bottom: 0.5rem">Possible causes for this error:</div>
+                        <div style="margin-left: 0.5rem; margin-bottom: 0.4rem; line-height: 1rem;">1) Given request URL is incorrect or invalid</div>
+                        <div style="margin-left: 0.5rem; margin-bottom: 0.4rem; line-height: 1rem;">2) The server for the url isn't returning a valid response for the created request</div>
+                        <div style="margin-left: 0.5rem; margin-bottom: 0.4rem; line-height: 1rem;">3) No CORS headers present for the requested url and requested http method (Can be bypassed using the <a href="https://chrome.google.com/webstore/detail/restfox-cors-helper/pgoncladmcclnmilkbnmmbldcihdgfnf" target="_blank">Chrome</a> or <a href="https://addons.mozilla.org/en-US/firefox/addon/restfox-cors-helper/" target="_blank">Firefox</a> extension for Restfox)</div>
+                        <div style="margin-left: 0.5rem; margin-bottom: 0.4rem; line-height: 1rem;">4) On the browser version, only https urls and localhost can be loaded at the moment. So if you're hitting a http url, the request will fail because https doesn't like requesting http urls.</div>
+                    </div>
+                </div>
             </template>
             <template v-if="activeResponsePanelTab === 'Header'">
                 <div class="content-box">
