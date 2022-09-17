0767290948aaeb29f4b0ceb7ddf44e36f82493d0
Flawid DSouza <me@flawiddsouza.com>
2022-09-17 13:43:34 +0530

    fix(ui): Response Panel > don't show "possible causes for error" when request is cancelled by the user

--- ui/src/components/ResponsePanel.vue
+++ ui/src/components/ResponsePanel.vue
@@ -43,7 +43,7 @@
                 </template>
                 <div class="content-box" v-else>
                     <div>{{ response.error }}</div>
-                    <div style="margin-top: 1.5rem; width: 30rem;">
+                    <div style="margin-top: 1.5rem; width: 30rem;" v-if="response.error !== 'Error: Request Cancelled'">
                         <div style="margin-bottom: 0.5rem">Possible causes for this error:</div>
                         <div style="margin-left: 0.5rem; margin-bottom: 0.4rem; line-height: 1rem;">1) Given request URL is incorrect or invalid</div>
                         <div style="margin-left: 0.5rem; margin-bottom: 0.4rem; line-height: 1rem;">2) The server for the url isn't returning a valid response for the created request</div>
