d35ac1437ea4653de22a1c7e6c04ea01a839132c
Flawid DSouza <me@flawiddsouza.com>
2022-09-18 12:54:29 +0530

    fix(ui): Sidebar > Folder & Request Settings Modal > Add footer with done button like in Environment modal, as it feels like there's no way to close the modal (currently you can click on the background to close the modal, but that might be unintuitive)

--- ui/src/components/modals/SidebarSettingsModal.vue
+++ ui/src/components/modals/SidebarSettingsModal.vue
@@ -6,6 +6,9 @@
                 <input type="text" style="width: 100%; border: 1px solid var(--default-border-color); outline: 0; padding: 0.3rem; background: inherit;" v-model="collectionItem.name" :placeholder="collectionItem._type === 'request_group' ? 'Folder Name' : 'Request Name'" spellcheck="false" v-focus>
             </label>
             <div style="padding-bottom: 1rem"></div>
+            <template #footer>
+                <button @click="showModalComp = false">Done</button>
+            </template>
         </modal>
     </div>
 </template>
