199254cd4d4f5aaba599d3e12e03c58efe7151d1
Flawid DSouza <me@flawiddsouza.com>
2022-09-20 10:45:23 +0530

    feat(ui): Environment Modal > Add a simple note to explain how to use the environment

--- ui/src/components/modals/EnvironmentModal.vue
+++ ui/src/components/modals/EnvironmentModal.vue
@@ -29,7 +29,12 @@
             </div>
 
             <template #footer>
-                <button>Done</button>
+                <div style="display: flex; justify-content: space-between; align-items: center;">
+                    <div style="font-size: 0.8rem; text-align: left; line-height: 1rem;" v-pre><span class="code">"key": "value"</span> pairs defined in the above JSON object can be referenced in any text input in the request panel using <span class="code">{{ key }}</span> for variable substitution</div>
+                    <div>
+                        <button>Done</button>
+                    </div>
+                </div>
             </template>
         </modal>
         <template v-if="showEnvironmentContextMenuPopup">
