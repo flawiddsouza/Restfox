2e638290396f91bc67c0a7a10d3755213b65b3dd
Flawid DSouza <me@flawiddsouza.com>
2022-11-06 09:58:46 +0530

    fix: github workflow for MacOS builds

--- packages/electron/forge.config.js
+++ packages/electron/forge.config.js
@@ -6,7 +6,7 @@ module.exports = {
         osxNotarize: {
             tool: 'notarytool',
             appleId: process.env.APPLE_ID,
-            appleIdPassword: process.env.APPLE_PASSWORD,
+            appleIdPassword: process.env.APPLE_ID_PASSWORD,
             teamId: process.env.APPLE_TEAM_ID
         }
     },
