b870e9bb94a65e2c7846d329a9f15200fdc66988
Flawid DSouza <me@flawiddsouza.com>
2022-11-19 10:16:10 +0530

    feat(ui): setup testing framework + add test for substituteEnvironmentVariables

---
+++ packages/ui/src/helpers.spec.js
@@ -0,0 +1,39 @@
+import { assert, test } from 'vitest'
+import { substituteEnvironmentVariables } from './helpers.ts'
+
+test('substituteEnvironmentVariables', () => {
+    const environment = {
+        "string": "cat",
+        "number": 4404,
+        "object": {
+            "string": "hey",
+            "array": [
+                "r1",
+                "r2",
+                "r3"
+            ]
+        }
+    }
+
+    const json = `{
+        "edge case": "{{",
+        "accessing object path array": "{{ object.array }}",
+        "accessing object path array item": "{{ object.array[0] }}",
+        "access object path string": "{{ object.string }}",
+        "access top level object key": "{{ string }}",
+        "number": {{ number }}
+    }`
+
+	const result = substituteEnvironmentVariables(environment, json)
+
+    const expectedResult = `{
+        "edge case": "{{",
+        "accessing object path array": "r1,r2,r3",
+        "accessing object path array item": "r1",
+        "access object path string": "hey",
+        "access top level object key": "cat",
+        "number": 4404
+    }`
+
+    assert.equal(result, expectedResult)
+})
