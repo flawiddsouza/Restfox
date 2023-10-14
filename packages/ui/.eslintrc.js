bbe91e98754dcbd227361a2dd13190e6f83b43ce
Flawid DSouza <me@flawiddsouza.com>
2023-10-14 12:12:36 +0530

    fix(ui): .estlintrc.js - allow template literals for strings - don't force single quotes when template literals might be a better choice

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -32,7 +32,8 @@ module.exports = {
         ],
         'quotes': [
             'error',
-            'single'
+            'single',
+            { 'allowTemplateLiterals': true }
         ],
         'semi': [
             'error',
