6d534f9ecae929e4dc5e3a46a4bc9936dfad8021
Flawid DSouza <me@flawiddsouza.com>
2023-11-07 15:28:41 +0530

    fix(ui): Copy as Curl > environment variables not substituted in body

--- packages/ui/src/utils/generate-code.ts
+++ packages/ui/src/utils/generate-code.ts
@@ -1,4 +1,4 @@
-import { createRequestData } from '@/helpers'
+import { createRequestData, substituteEnvironmentVariables } from '@/helpers'
 import { HTTPSnippet } from 'httpsnippet-browser'
 
 export async function generateCode(request, environment, target: 'shell', clientId: 'curl') {
@@ -18,6 +18,30 @@ export async function generateCode(request, environment, target: 'shell', client
     if (har.postData.mimeType) {
         if(har.postData.mimeType === 'application/x-www-form-urlencoded' || har.postData.mimeType === 'multipart/form-data') {
             har.postData.params = har.postData.params.filter(param => !param.disabled)
+        }
+
+        if(har.postData.mimeType === 'application/x-www-form-urlencoded') {
+            har.postData.params = har.postData.params.map(item => {
+                return {
+                    name: substituteEnvironmentVariables(environment, item.name),
+                    value: substituteEnvironmentVariables(environment, item.value)
+                }
+            })
+        }
+
+        if(har.postData.mimeType === 'multipart/form-data') {
+            har.postData.params.forEach(param => {
+                if(param.type === 'text') {
+                    param.name = substituteEnvironmentVariables(environment, param.name)
+                    param.value = substituteEnvironmentVariables(environment, param.value)
+                } else {
+                    param.name = substituteEnvironmentVariables(environment, param.name)
+                }
+            })
+        }
+
+        if(har.postData.mimeType === 'text/plain' || har.postData.mimeType === 'application/json' || har.postData.mimeType === 'application/graphql') {
+            har.postData.text = substituteEnvironmentVariables(environment, har.postData.text)
         }
     }
 
