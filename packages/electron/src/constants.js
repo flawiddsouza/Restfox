1d69ddeda7b43f104271361b3d6daa5efcdd3d6d
Flawid DSouza <me@flawiddsouza.com>
2024-03-17 13:36:03 +0530

    fix(electron+ui): Workspace > In Filesystem (Git Friendly) > change root config from _.json to restfox.json

--- packages/electron/src/constants.js
+++ packages/electron/src/constants.js
@@ -1,7 +1,7 @@
 module.exports = {
     DEFAULT_ENVIRONMENT: 'Default',
     FILES: {
-        WORKSPACE_CONFIG: '_.json',
+        WORKSPACE_CONFIG: 'restfox.json',
         FOLDER_CONFIG: '_.json',
         WORKSPACE_PLUGINS: '_.plugins.json',
         FOLDER_PLUGINS: '_.plugins.json',
