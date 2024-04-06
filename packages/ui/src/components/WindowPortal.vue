e70de33f1886c3cd5ea4e8c4315385fb97556959
Flawid DSouza <me@flawiddsouza.com>
2024-04-06 10:18:48 +0530

    fix(ui): Move to New Window > Icons not loading

--- packages/ui/src/components/WindowPortal.vue
+++ packages/ui/src/components/WindowPortal.vue
@@ -6,26 +6,10 @@
 
 <script>
 function copyStyles(sourceDoc, targetDoc) {
-    Array.from(sourceDoc.styleSheets).forEach(styleSheet => {
-        if (styleSheet.cssRules) {
-            // for <style> elements
-            const newStyleEl = sourceDoc.createElement('style')
-
-            Array.from(styleSheet.cssRules).forEach(cssRule => {
-                // write the text of each rule into the body of the style element
-                newStyleEl.appendChild(sourceDoc.createTextNode(cssRule.cssText))
-            })
-
-            targetDoc.head.appendChild(newStyleEl)
-        } else if (styleSheet.href) {
-            // for <link> elements loading CSS from a URL
-            const newLinkEl = sourceDoc.createElement('link')
-
-            newLinkEl.rel = 'stylesheet'
-            newLinkEl.href = styleSheet.href
-            targetDoc.head.appendChild(newLinkEl)
-        }
-    })
+    for (const el of sourceDoc.head.querySelectorAll('style, link[rel=stylesheet]')) {
+        const clone = el.cloneNode(true)
+        targetDoc.head.appendChild(clone)
+    }
 }
 
 export default {
