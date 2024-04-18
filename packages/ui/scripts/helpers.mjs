5922c3f2610435813f5a23676fc16080f81ed4f0
Flawid DSouza <me@flawiddsouza.com>
2024-04-18 20:50:09 +0530

    fix(ui): scripts > __dirname from helpers returns incorrect __dirname on Windows

--- packages/ui/scripts/helpers.mjs
+++ packages/ui/scripts/helpers.mjs
@@ -1,7 +1,9 @@
 import { readdirSync, statSync } from 'fs'
 import path from 'path'
+import { fileURLToPath } from 'url'
 
-export const __dirname = import.meta.url.split('/').slice(2,-1).join('/')
+const __filename = fileURLToPath(import.meta.url)
+export const __dirname = path.dirname(__filename)
 
 export function humanFileSize(size) {
     var i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
