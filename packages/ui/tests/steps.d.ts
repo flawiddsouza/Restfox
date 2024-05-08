5f642b213832c0f11a685c84a83c7bf5bf569c42
kobenguyent <kobenguyent@gmail.com>
2024-05-08 13:17:46 +0200

    feat(test): e2e tests with codeceptjs

---
+++ packages/ui/tests/steps.d.ts
@@ -0,0 +1,11 @@
+/// <reference types='codeceptjs' />
+type steps_file = typeof import('./steps_file');
+
+declare namespace CodeceptJS {
+  interface SupportObject { I: I, current: any }
+  interface Methods extends Playwright, ExpectHelper {}
+  interface I extends ReturnType<steps_file>, WithTranslation<Methods> {}
+  namespace Translation {
+    interface Actions {}
+  }
+}
---
+++ packages/ui/tests/steps_file.ts
@@ -0,0 +1,34 @@
+export = function() {
+    return actor({
+
+        creatRequest(requestName: string) {
+            this.rightClick('.sidebar')
+            this.waitForText('New Request')
+            this.click('New Request')
+            this.fillField('input[placeholder="My Request"]', requestName)
+            this.click('Create')
+        },
+
+        async isActiveSidebarItem(name: string) {
+            const text = await this.grabTextFrom('.sidebar-item.sidebar-item-active')
+            this.expectEqual(text.trim() === name, true)
+        },
+
+        async isActiveTab(name: string) {
+            const text = await this.grabTextFrom('.tab.tab-active')
+            this.expectEqual(text.trim() === name, true)
+        },
+
+        typeInRequestPanelAddressBar(url: string, clear = false) {
+            this.click({ pw: '[data-testid="request-panel-address-bar"]' })
+
+            if(clear) {
+                this.pressKey(['CommandOrControl', 'A'])
+                this.pressKey('Backspace')
+            }
+
+            this.type(url)
+        }
+
+    })
+}
