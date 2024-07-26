34b295ff1706c2cf3f6dd8bcf32ed0ad7b410771
KobeN <7845001+kobenguyent@users.noreply.github.com>
2024-07-26 10:31:57 +0200

    feat(ui): Request Panel > Docs tab > existence indicator (#197)

--- packages/ui/src/components/RequestPanelTabTitle.vue
+++ packages/ui/src/components/RequestPanelTabTitle.vue
@@ -1,5 +1,6 @@
 <template>
     <span>{{ requestPanelTab.name }}<i class="fa fa-circle active-script" v-if="scriptIndicator && requestPanelTab.name === 'Script'" style="margin-left: 0.2rem"></i></span>
+    <i class="fa fa-circle active-script" v-if="docIndicator && requestPanelTab.name === 'Docs'" style="margin-left: 0.2rem"></i>
     <template v-if="requestPanelTab.name === 'Body'">
         <template v-if="activeTab.body.mimeType === 'application/x-www-form-urlencoded'">
             <template v-if="'params' in activeTab.body && activeTab.body.params.filter(item => item.disabled === undefined || item.disabled === false).length > 0">
@@ -45,6 +46,9 @@ export default {
             required: true
         },
         scriptIndicator: {
+            type: Boolean
+        },
+        docIndicator: {
             type: Boolean
         }
     },
