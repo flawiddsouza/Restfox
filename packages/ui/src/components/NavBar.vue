b4fc6934575069001c0b300e971d40139b7ba42c
KobeN <7845001+kobenguyent@users.noreply.github.com>
2024-08-04 07:13:30 +0200

    fix: using string literal

--- packages/ui/src/components/NavBar.vue
+++ packages/ui/src/components/NavBar.vue
@@ -275,7 +275,7 @@ export default {
             const list =  this.environments.map(item => {
                 return {
                     type: 'option',
-                    label: `&nbsp;<i class="fa fa-circle" style="color:$ {item.color}"></i> ${item.name}',
+                    label: `&nbsp;<i class="fa fa-circle" style="color:$ {item.color}"></i> ${item.name}`,
                     value: `${item.name}`,
                     class: 'context-menu-item-with-left-padding'
                 }
