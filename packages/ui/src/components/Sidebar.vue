d65620c82257ddd85e770889470ccd89c2e58ac3
Lucas Almeida <39913588+lucas-almeida026@users.noreply.github.com>
2025-05-11 10:07:55 -0300

    handle drag over

--- packages/ui/src/components/Sidebar.vue
+++ packages/ui/src/components/Sidebar.vue
@@ -398,34 +398,53 @@ export default {
             this.draggedSidebarElement.style.opacity = ''
             this.draggedSidebarElement = null
         },
+
         dragOver(event) {
             if(!this.draggedSidebarElement) {
                 return
             }
-            const sidebarItemToDropOn = event.target.closest('.sidebar-item')
-            if(!sidebarItemToDropOn) {
+            let container
+            const sidebarItem = event.target.closest('.sidebar-item')
+            const sidebarListContainer = event.target.closest('.sidebar-list-container')
+            if (sidebarItem) {
+                container = {
+                    type: 'sidebar-item',
+                    element: sidebarItem
+                }
+            } else if (sidebarListContainer) {
+                const innerList = sidebarListContainer.querySelector('.sidebar-list')
+                if (!innerList) {
+                    return
+                }
+                container = {
+                    type: 'sidebar-list',
+                    element: innerList
+                }
+            } else {
                 return
             }
-            const rect = sidebarItemToDropOn.getBoundingClientRect()
+
+            const elementToDropOn = container.element
+            const rect = elementToDropOn.getBoundingClientRect()
             const offset = rect.top + document.body.scrollTop
-            const elementHeight = parseFloat(getComputedStyle(sidebarItemToDropOn, null).height.replace('px', ''))
+            const elementHeight = parseFloat(getComputedStyle(elementToDropOn, null).height.replace('px', ''))
             const y = event.pageY
             const location = Math.abs(offset - y)
             if (location < (elementHeight / 2)) {
                 this.sidebarItemCursorPosition = 'top'
-                sidebarItemToDropOn.style.borderTop = '1px dashed var(--text-color)'
-                sidebarItemToDropOn.style.borderBottom = ''
-                sidebarItemToDropOn.style.backgroundColor = ''
+                elementToDropOn.style.borderTop = '1px dashed var(--text-color)'
+                elementToDropOn.style.borderBottom = ''
+                elementToDropOn.style.backgroundColor = ''
             } else {
                 this.sidebarItemCursorPosition = 'bottom'
-                if(sidebarItemToDropOn.dataset.type === 'request_group') {
-                    sidebarItemToDropOn.style.borderTop = ''
-                    sidebarItemToDropOn.style.borderBottom = ''
-                    sidebarItemToDropOn.style.backgroundColor = 'var(--drop-target-background-color)'
+                if(elementToDropOn.dataset.type === 'request_group') {
+                    elementToDropOn.style.borderTop = ''
+                    elementToDropOn.style.borderBottom = ''
+                    elementToDropOn.style.backgroundColor = 'var(--drop-target-background-color)'
                 } else {
-                    sidebarItemToDropOn.style.borderTop = ''
-                    sidebarItemToDropOn.style.borderBottom = '1px dashed var(--text-color)'
-                    sidebarItemToDropOn.style.backgroundColor = ''
+                    elementToDropOn.style.borderTop = ''
+                    elementToDropOn.style.borderBottom = '1px dashed var(--text-color)'
+                    elementToDropOn.style.backgroundColor = ''
                 }
             }
             event.preventDefault()
@@ -434,11 +453,32 @@ export default {
             if(!this.draggedSidebarElement) {
                 return
             }
-            const sidebarItemToDropOn = event.target.closest('.sidebar-item')
-            if(sidebarItemToDropOn) {
-                sidebarItemToDropOn.style.borderBottom = ''
-                sidebarItemToDropOn.style.borderTop = ''
-                sidebarItemToDropOn.style.backgroundColor = ''
+            let container
+            const sidebarItem = event.target.closest('.sidebar-item')
+            const sidebarListContainer = event.target.closest('.sidebar-list-container')
+            console.log(sidebarItem, sidebarListContainer)
+            if(sidebarItem) {
+                container = {
+                    type: 'sidebar-item',
+                    element: sidebarItem
+                }
+            } else if(sidebarListContainer) {
+                const innerList = sidebarListContainer.querySelector('.sidebar-list')
+                if (!innerList) {
+                    return
+                }
+                container = {
+                    type: 'sidebar-list',
+                    element: innerList
+                }
+            } else {
+                return
+            }
+            const elementToDropOn = container.element
+            if(elementToDropOn) {
+                elementToDropOn.style.borderBottom = ''
+                elementToDropOn.style.borderTop = ''
+                elementToDropOn.style.backgroundColor = ''
             }
         },
         drop(event) {
@@ -446,11 +486,32 @@ export default {
                 return
             }
             event.preventDefault()
-            const sidebarItemToDropOn = event.target.closest('.sidebar-item')
-            if(sidebarItemToDropOn) {
-                sidebarItemToDropOn.style.borderTop = ''
-                sidebarItemToDropOn.style.borderBottom = ''
-                sidebarItemToDropOn.style.backgroundColor = ''
+            let container
+            const sidebarItem = event.target.closest('.sidebar-item')
+            const sidebarListContainer = event.target.closest('.sidebar-list-container')
+            if (sidebarItem) {
+                container = {
+                    type: 'sidebar-item',
+                    element: sidebarItem
+                }
+            } else if (sidebarListContainer) {
+                const innerList = sidebarListContainer.querySelector('.sidebar-list')
+                if (!innerList) {
+                    return
+                }
+                container = {
+                    type: 'sidebar-list',
+                    element: innerList
+                }
+            } else {
+                return
+            }
+
+            const elementToDropOn = container.element
+            if(elementToDropOn) {
+                elementToDropOn.style.borderTop = ''
+                elementToDropOn.style.borderBottom = ''
+                elementToDropOn.style.backgroundColor = ''
 
                 this.$store.dispatch('reorderCollectionItem', {
                     from: {
@@ -458,9 +519,9 @@ export default {
                         id: this.draggedSidebarElement.dataset.id
                     },
                     to: {
-                        parentId: sidebarItemToDropOn.dataset.parentId,
-                        id: sidebarItemToDropOn.dataset.id,
-                        type: sidebarItemToDropOn.dataset.type,
+                        parentId: elementToDropOn.dataset.parentId,
+                        id: elementToDropOn.dataset.id,
+                        type: elementToDropOn.dataset.type,
                         cursorPosition: this.sidebarItemCursorPosition
                     }
                 })
@@ -575,6 +636,7 @@ export default {
 </script>
 
 <style>
+
 .sidebar .sidebar-filter {
     display: flex;
     border-bottom: 1px solid var(--default-border-color);
