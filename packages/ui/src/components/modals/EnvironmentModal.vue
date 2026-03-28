a897b82912c9c1efd0f1a3ae1c22979d9ea1d60a
Rafael Roseira Machado <rafaroseira27@outlook.com>
2026-03-28 16:59:35 -0300

    feat(ui): reorder environments

--- packages/ui/src/components/modals/EnvironmentModal.vue
+++ packages/ui/src/components/modals/EnvironmentModal.vue
@@ -14,8 +14,27 @@
                 <div class="env-sidebar-col">
                     <button class="button" type="button" style="margin-bottom: 0.5rem; margin-right: 0.5rem;" @click="addEnvironment()">Add Environment</button>
                     <div style="overflow-y: auto;" class="environment-sidebar">
-                        <div v-for="environment in environments" class="environment-sidebar-item" :class="{ 'environment-sidebar-item-active': environment.name === currentEnvironment }" @click="changeEnvironment(environment)" :ref="'environment-' + environment.name">
-                            <div><i class="fa fa-circle" :style="{ color: environment.color, marginRight: '0.5rem' }"></i>{{ environment.name }}</div>
+                        <div v-for="(environment, index) in environments" :key="environment.name" 
+                            class="environment-sidebar-item" 
+                            :class="{ 'environment-sidebar-item-active': environment.name === currentEnvironment }" 
+                            @click="changeEnvironment(environment)" 
+                            :ref="'environment-' + environment.name"
+                            draggable="true"
+                            @mousedown="onMouseDown"
+                            @dragstart="onDragStart(index, $event)"
+                            @dragover.prevent
+                            @dragenter.prevent
+                            @drop="onDrop(index)"
+                            @dragend="onDragEnd">
+                            <div style="display: flex; align-items: center;">
+                                <div class="drag-handle" style="display: flex; align-items: center; margin-right: 0.3rem;" @mouseenter="dragEnabledIndex = index" @mouseleave="dragEnabledIndex = null">
+                                    <svg viewBox="0 0 24 24" width="14" height="14" style="fill: var(--text-color);">
+                                        <path d="M11 18c0 1.1-.9 2-2 2s-2-.9-2-2 .9-2 2-2 2 .9 2 2zm-2-8c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm6 4c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path>
+                                    </svg>
+                                </div>
+                                <i class="fa fa-circle" :style="{ color: environment.color, marginRight: '0.5rem' }"></i>{{ environment.name }}
+                            </div>
+                           
                             <div class="environment-sidebar-item-menu" :class="{ 'environment-sidebar-item-menu-disable-hide': environment.name === clickedContextMenuEnvironment.name && showEnvironmentContextMenuPopup === true }" @click.stop="showEnvironmentContextMenu($event, environment)">
                                 <svg viewBox="0 0 24 24" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;">
                                     <g>
@@ -106,6 +125,8 @@ export default {
             editTagModalShow: false,
             editTagParsedFunc: null,
             editTagUpdateFunc: null,
+            draggedEnvironmentIndex: null,
+            isHandleClicked: false
         }
     },
     computed: {
@@ -305,6 +326,34 @@ export default {
             if(manuallyTriggerSave) {
                 this.saveEnvironment()
             }
+        },
+        onMouseDown(event) {
+            this.isHandleClicked = !!event.target.closest('.drag-handle')
+        },
+        onDragStart(index, event) {
+            if (!this.isHandleClicked) {
+                event.preventDefault()
+                return
+            }
+
+            this.draggedEnvironmentIndex = index
+            if (event.dataTransfer) {
+                event.dataTransfer.effectAllowed = 'move'
+                event.dataTransfer.setData('text/plain', index)
+            }
+        },
+        onDrop(dropIndex) {
+            if (this.draggedEnvironmentIndex === null || this.draggedEnvironmentIndex === dropIndex) {
+                return
+            }
+
+            const draggedItem = this.environments.splice(this.draggedEnvironmentIndex, 1)[0]
+            this.environments.splice(dropIndex, 0, draggedItem)
+            this.saveEnvironments()
+            this.draggedEnvironmentIndex = null
+        },
+        onDragEnd() {
+            this.draggedEnvironmentIndex = null
         },
         saveEnvironment() {
             if(this.collectionItem) {
@@ -605,6 +654,24 @@ export default {
 .environment-sidebar-item:not(.environment-sidebar-item-selected) > .environment-sidebar-item-menu:hover,
 .environment-sidebar-item:not(.environment-sidebar-item-selected) > .environment-sidebar-item-menu.environment-sidebar-item-menu-disable-hide {
     background-color: rgb(108 194 197 / 20%);
+}
+
+.environment-sidebar-item .drag-handle {
+    opacity: 0;
+    cursor: grab;
+    transition: opacity 0.1s ease-in-out;
+}
+
+.environment-sidebar-item:hover .drag-handle {
+    opacity: 0.5;
+}
+
+.environment-sidebar-item .drag-handle:hover {
+    opacity: 1;
+}
+
+.environment-sidebar-item .drag-handle:active {
+    cursor: grabbing;
 }
 
 .context-menu-background-overlay {
